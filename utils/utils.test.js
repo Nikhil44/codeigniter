b09f108c9b0b12b214c865a97b4532cb32513cb6
nik44 <nbang0506@gmail.com>
2017-12-03 16:00:14 +0530

    integration of assert and asyn test

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,19 +1,33 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 
 it('should add two number',() => {
 var res = utils.add(33,11);
-if(res !== 44 )
-{
-throw new Error('Value not correct');
-}
+expect(res).toBe(44);
 });
+
+it('should async add two number',(done) => {
+  utils.asyncAdd(4,3, (sum) =>{
+  expect(sum).toBe(7);
+  done();
+  });
+});
+
+
+
 
 
 it('should return square',() => {
 var res = utils.square(5);
-if(res !== 25 )
-{
-throw new Error('Value not correct');
-}
+expect(res).toBe(25);
+});
+
+it('should return square',() => {
+var user = {location: 'phil', age: 25};
+var res = utils.setName(user, 'nikhil bang');
+expect(res).toInclude({
+  firstName: 'nikhil',
+  lastName: 'bang'
+});
 });
