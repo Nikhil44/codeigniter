84a48cda5671d22d2e722b10cf5955d01e764b73
nik44 <nbang0506@gmail.com>
2017-12-06 13:37:28 +0530

    connection with mongo

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').insertOne({
+    name: 'nikhil',
+    age: 25,
+    location: 'nikhil bang'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+
+    console.log(result.ops);
+  });
+
+  db.close();
+});
