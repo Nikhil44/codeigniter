219bb985d89fd89624cc2a13f85e85fd3bf1bf5e
nik44 <nbang0506@gmail.com>
2017-12-06 14:01:16 +0530

    Destructuring and Object id

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,22 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
 
+//Destructuring assignment
+
+const {MongoClient, ObjectID} = require('mongodb');
+console.log(new ObjectID());
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
-
-  db.collection('Users').insertOne({
-    name: 'nikhil',
-    age: 25,
-    location: 'nikhil bang'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert user', err);
-    }
-
-    console.log(result.ops);
-  });
 
   db.close();
 });
