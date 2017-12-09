6a55ebb9b1bd9f928dc9e8b61ac9eda0d11e088f
nik44 <nbang0506@gmail.com>
2017-12-09 17:48:27 +0530

    validation on mongo model

--- server/server.js
+++ server/server.js
@@ -6,13 +6,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp')
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type:String
+    type:String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type:Number
+    type:Number,
+    default:null
   }
 });
 
@@ -26,17 +31,46 @@ newTodo.save().then((doc) => {
   console.log('unable to save Todo')
 })
 
-
-
 var otherTodo = new Todo ({
   text: 'go to gym',
   completed: 'true',
   completedAt: 123
 })
 
-
 otherTodo.save().then((doc) => {
   console.log(JSON.stringify(doc,undefined,2));
+},(e) => {
+  console.log('unable to save', e);
+});
+
+
+
+
+//user
+var User = mongoose.model('User', {
+email : {
+    type:String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type:Number,
+    default:null
+  }
+});
+
+var user = new User({
+  email: 'abcd@gmail.com'
+})
+
+
+user.save().then((doc) => {
+  console.log('user saved',doc);
 },(e) => {
   console.log('unable to save', e);
 });
