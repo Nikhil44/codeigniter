d919e06861ab6b1b85376cc5177b5ea573c86f5d
nik44 <nbang0506@gmail.com>
2017-12-11 14:31:59 +0530

    api of post and get with test case

--- server/server.js
+++ server/server.js
@@ -1,76 +1,55 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-mongoose.Promise = global.Promise;
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+var app = express();
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type:String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type:Number,
-    default:null
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-var newTodo = new Todo({
-  text: 'wakeup in the morning'
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
-newTodo.save().then((doc) => {
-  console.log('save todo',doc);
-}, (e) => {
-  console.log('unable to save Todo')
-})
-
-var otherTodo = new Todo ({
-  text: 'go to gym',
-  completed: 'true',
-  completedAt: 123
-})
-
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc,undefined,2));
-},(e) => {
-  console.log('unable to save', e);
-});
-
-
-
-
-//user
-var User = mongoose.model('User', {
-email : {
-    type:String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type:Number,
-    default:null
-  }
-});
-
-var user = new User({
-  email: 'abcd@gmail.com'
-})
-
-
-user.save().then((doc) => {
-  console.log('user saved',doc);
-},(e) => {
-  console.log('unable to save', e);
-});
+module.exports = {app};
