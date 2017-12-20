796da2a91fc564ec7680185f8fa74b866e20cfe8
nik44 <nbang0506@gmail.com>
2017-12-20 13:16:45 +0530

    use of Broadcast

--- server/server.js
+++ server/server.js
@@ -14,33 +14,35 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newEmail', {
-  from: 'nikhil@example.com',
-  text: 'Hey.ram ram',
-  createAt: 123
-  });
-
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
-  });
-
-
-
   socket.emit('newMessage', {
-     from: 'nikhil',
-     text: 'See you then',
-     createdAt: 123123
-   });
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
-   socket.on('createMessage', (message) => {
-     console.log('createMessage', message);
-   });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
-
-
 });
 
 server.listen(port, () => {
